c53e1c77718a1464329be4edea924e325168b828
juan12255 <dmartinez4463@misena.edu.co>
2022-11-02 11:36:23 -0500

    creacion de vistas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,10 +54,6 @@
                         <div class="flex items-center"> 
                         </div>
                     </div>
-
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                    </div>
                 </div>
             </div>
         </div>
